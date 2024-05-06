7a4722adadc06dfe1177fa81cee3550c4cfb115d
phamgiang01 <77712988+phamgiang01@users.noreply.github.com>
2024-05-06 23:18:48 +0700

    Update test.py

--- test.py
+++ test.py
@@ -79,10 +79,6 @@ def predict_diseases():
 
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello!</h1>"
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
